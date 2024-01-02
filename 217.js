4265451f1c4de43f9c5fa265ad729f3048fd6d02
shokhrukhKarimov <shohruxkarimov546@gmail.com>
2024-01-02 10:27:01 +0500

    217. Contains Duplicate

--- 217.js
+++ 217.js
@@ -1,3 +1,7 @@
+/*
+https://leetcode.com/problems/contains-duplicate/description/
+*/
+
 const containsDuplicate = function (nums) {
   const newArr = Array.from(new Set(nums));
   return nums.length !== newArr.length;
