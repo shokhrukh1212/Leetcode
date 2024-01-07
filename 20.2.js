73b69b90e1132bf4fac0995653b8da12afaaf521
shokhrukhKarimov <shohruxkarimov546@gmail.com>
2024-01-07 14:05:32 +0500

    70. Climbing Stairs / dynamic programming

---
+++ 20.2.js
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = function (n) {
+  if (n < 2) {
+    return 1;
+  }
+
+  let firstStep = 1;
+
+  let secondStep = 1;
+
+  let thirdStep = 0;
+
+  for (let i = 2; i <= n; i++) {
+    thirdStep = firstStep + secondStep;
+
+    firstStep = secondStep;
+
+    secondStep = thirdStep;
+  }
+  return thirdStep;
+};
+
+console.log(climbStairs(6));
+// 13
