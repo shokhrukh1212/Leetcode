273d3bc847aa333b0324cc9f5b016e047798644b
shokhrukhKarimov <shohruxkarimov546@gmail.com>
2024-01-25 19:51:34 +0500

    350. Intersection of Two Arrays || / using hashmap

---
+++ 38.2.js
@@ -0,0 +1,20 @@
+const intersect = function (nums1, nums2) {
+  const map = new Map();
+  for (const n of nums1) {
+    if (map.has(n)) {
+      map.set(n, map.get(n) + 1);
+    } else {
+      map.set(n, 1);
+    }
+  }
+  const result = [];
+  for (const n of nums2) {
+    if (map.has(n) && map.get(n) > 0) {
+      result.push(n);
+      map.set(n, map.get(n) - 1);
+    }
+  }
+  return result;
+};
+
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
