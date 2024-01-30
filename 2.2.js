197aa7dee851e4f189ae204ae74843cb13fe6a8e
shokhrukhKarimov <shohruxkarimov546@gmail.com>
2024-01-30 22:53:16 +0500

    189. Rotate Array

---
+++ 2.2.js
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/rotate-array/
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotate = function (nums, k) {
+  const n = nums.length;
+  k %= n;
+
+  const rotated = [];
+  for (let i = 0; i < n; i++) {
+    rotated[(i + k) % n] = nums[i];
+  }
+
+  for (let i = 0; i < n; i++) {
+    nums[i] = rotated[i];
+  }
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
