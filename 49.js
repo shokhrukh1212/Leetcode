f09e0e0a8262384d28102a38f3adcd62dc141666
shokhrukhKarimov <shohruxkarimov546@gmail.com>
2024-02-05 21:56:51 +0500

    344. Reverse String

---
+++ 49.js
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/reverse-string/
+ */
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+const reverseString = function (s) {
+  return s.reverse();
+};
