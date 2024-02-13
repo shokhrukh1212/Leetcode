e46d50c4e7dda0e5aa6acdfac4e41de1d2d96202
shokhrukhKarimov <shohruxkarimov546@gmail.com>
2024-02-13 15:36:30 +0500

    12. Integer to Roman / faster

---
+++ 7.2.js
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/integer-to-roman/
+ */
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const rom = [
+  "M",
+  "CM",
+  "D",
+  "CD",
+  "C",
+  "XC",
+  "L",
+  "XL",
+  "X",
+  "IX",
+  "V",
+  "IV",
+  "I",
+];
+
+const intToRoman = function (N) {
+  let ans = "";
+  for (let i = 0; N; i++) while (N >= val[i]) (ans += rom[i]), (N -= val[i]);
+  return ans;
+};
